3d05870b77ed16da7a2eb3cfcb2db8bd069f1179
Celine <futuremakersto@gmail.caom>
2025-07-15 11:02:47 -0400

    Initial Commit

---
+++ StudyWell/StudyWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyWellApp.swift
+//  StudyWell
+//
+//  Created by Future Makers on 2025-07-15.
+//
+
+import SwiftUI
+
+@main
+struct StudyWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyWellTests/StudyWellTests.swift
@@ -0,0 +1,16 @@
+//
+//  StudyWellTests.swift
+//  StudyWellTests
+//
+//  Created by Future Makers on 2025-07-15.
+//
+
+import Testing
+
+struct StudyWellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
