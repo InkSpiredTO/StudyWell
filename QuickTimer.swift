caa8917b97540038d5d5f9cdcfc52480758feddb
Celine <futuremakersto@gmail.caom>
2025-07-16 13:14:54 -0400

    Quick timer created with some minor bugs.

--- QuickTimer.swift
+++ QuickTimer.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 
 struct QuickTimer: View {
+    @State private var buttonText = "Start Timer"
     @State private var timeset = 0
-    @State private var timeRemaining = 100
+    @State private var timeRemaining = 0
+    @State private var TimerOn = 0
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     let step = 1
@@ -61,13 +63,17 @@ struct QuickTimer: View {
                     .padding(.trailing, 40.0)
                     .padding(.leading)
                     
-                            .onReceive(timer) { _ in
-                                if timeRemaining > 0 {
+                        .onReceive(timer) { _ in
+                            print("Signal Recived")
+                            if timeRemaining > 0 {
+                                if TimerOn == 1 {
                                     timeRemaining -= 1 // decrement the time by 1 second
-                                } else {
-                                    timer.upstream.connect().cancel() // stop the timer
                                 }
+                            } else {
+                                timer.upstream.connect().cancel() // stop the timer
+                                TimerOn = 0
                             }
+                        }
                     
                     Stepper (
                                 value: $timeRemaining,
@@ -76,10 +82,25 @@ struct QuickTimer: View {
                             ) {
                                 Text("Time remaining:  \(timeRemaining) sec")
                             }
-                            .padding(10)
+                            .padding(50)
                     
-                    Button("Start Timer") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    Button {
+                        if TimerOn == 0 {
+                            buttonText = "Stop"
+                            TimerOn = 1
+                        } else if TimerOn == 1 {
+                            buttonText = "Start Timer"
+                            TimerOn = 0
+                        }
+                    } label: {
+                        Text(buttonText)
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding(.horizontal, 40)
+                            .padding(.vertical, 20.0)
+                            .background(Color(red: 0.9372549019607843, green: 0.7254901960784313, blue: 0.796078431372549))
+                            .foregroundColor(Color.black)
+                            .cornerRadius(30)
                     }
                     
                     Spacer()
@@ -91,7 +112,6 @@ struct QuickTimer: View {
         }
         
     }
-    
 }
 
 #Preview {
